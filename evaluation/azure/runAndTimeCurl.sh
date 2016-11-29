d8e76c92e201bd1170f2335fa4ad4603baf2e2bb
adbhat <adithya.bhatp2@gmail.com>
2016-11-29 14:23:27 -0600

    https to http in loop

--- evaluation/azure/runAndTimeCurl.sh
+++ evaluation/azure/runAndTimeCurl.sh
@@ -1,4 +1,4 @@
-#./util/start-cluster.py
+
 #./util/setup.py --appdir pychat --appfile lambda_func.py
 
 #url=`head -2 applications/pychat/static/config.json | tail -1 | awk -F '":' '{print $2}' | xargs`
@@ -21,7 +21,7 @@ if [ -n "$2" ]
 then
 	url=$2
 else
-	url=https://nehatesting.azurewebsites.net/api/HttpTriggerCSharp1?code=d1vcyncMHpLajNlaxOYiPJZVsTLTp3ruQ/gXiRgdilEB6SlgP4hbqQ==
+	url=http://nehatesting.azurewebsites.net/api/HttpTriggerCSharp1?code=d1vcyncMHpLajNlaxOYiPJZVsTLTp3ruQ/gXiRgdilEB6SlgP4hbqQ==
 fi
 
 time curl -H "Content-Type: application/json" -X POST -s -w "\n" ${url} -d '{"name": "'"${msg}"'"}'
