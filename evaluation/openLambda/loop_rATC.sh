8cb38b98f2aa565d6a705a4e29db497d0ba09ea5
Vishakha Dhelia <vish.dhelia@gmail.com>
2016-11-29 19:08:45 +0000

    Observing 504 Gateway Timeout on openLambda

--- evaluation/openLambda/loop_rATC.sh
+++ evaluation/openLambda/loop_rATC.sh
@@ -8,7 +8,7 @@
 numReqs=10
 url=`head -2 ~/gitRepository/Lambdas/applications/pychat/static/config.json | tail -1 | awk -F '":' '{print $2}' | xargs`
 #xargs trims space and quotes
-
+ext=""
 
 if [ -z "$1" ]
 then
@@ -17,7 +17,13 @@ else
 	numReqs=$1
 fi
 
-outfile="output_loop_${numReqs}.txt"
+if [ $2 == "-v" ]
+then
+	echo "-v param passed : verbose.. output not trimmed"
+	ext="_v"
+fi
+
+outfile="output_loop_${numReqs}${ext}.txt"
 rm -f ${outfile}
 
 echo URL is:${url}
@@ -26,6 +32,15 @@ echo outfile is:${outfile}
 
 echo "Result\t\tTimeType\tTime" >> ${outfile}
 
+if [ $2 == "-v" ]
+then
+	for i in `seq 1 ${numReqs}`;
+	do
+	        #echo $i
+	        echo "`./runAndTimeCurl.sh "msg_${i}" ${url} 2>&1 `" >> $outfile & 
+	done
+	exit
+fi
 
 for i in `seq 1 ${numReqs}`;
 do
